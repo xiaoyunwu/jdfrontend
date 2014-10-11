d57bfcf38481b77454d1ee06392f1b23d7c6d9fb
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2014-10-10 17:31:54 -0700

    initial version of mixer

---
+++ mixer.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "net/http"
+    "net/url"
+    "io"
+    "log"
+)
+
+
+func main() {
+    tr := &http.Transport{
+        DisableCompression: true,
+        DisableKeepAlives: false,
+        MaxIdleConnsPerHost: 48,
+    }
+    
+    client := &http.Client{Transport: tr}
+
+    http.HandleFunc("/jds", func(w http.ResponseWriter, r *http.Request) {
+        proxy(w, r, client)
+    }) 
+
+    err := http.ListenAndServe(":9090", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
+
+// For now, we just need to copy the parameters over and copy the result back.
+// Late, we can add cache support, and also a/b testing support, and ads mixing
+// support. We also need to add tracking support.
+func proxy(w http.ResponseWriter, r *http.Request, client *http.Client) {
+        r.ParseForm()
+        var lurl *url.URL
+        lurl, err := url.Parse("http://localhost:9200")
+        lurl.Path += "/jidian/_jds"
+        parameters := url.Values{}
+      
+        for k, vs := range r.Form {
+            for _, v := range vs {
+                parameters.Add(k, v)
+            }
+        }
+        
+        lurl.RawQuery = parameters.Encode()
+
+        req, err := http.NewRequest("GET", lurl.String(), nil)
+        if err != nil {
+            log.Fatal(err)
+        }       
+        resp, err := client.Do(req)
+        if err != nil {
+            log.Fatal(err)
+        }       
+
+        copyHeaders(w.Header(), resp.Header)
+        w.WriteHeader(resp.StatusCode)
+        _, err = io.Copy(w, resp.Body)
+        if err := resp.Body.Close(); err != nil {
+            log.Fatal("Can't close response body %v", err)
+        }
+}  
+
+func copyHeaders(dst, src http.Header) {
+    for k, _ := range dst {
+        dst.Del(k)
+    }
+    for k, vs := range src {
+        for _, v := range vs {
+            dst.Add(k, v)
+        }
+    }
+}
